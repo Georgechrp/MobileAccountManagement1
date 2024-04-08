1b86f62950ba647b76c06c3025e7b6bb302a3274
Georgechrp <159652944+Georgechrp@users.noreply.github.com>
2024-04-08 17:33:54 +0300

    + Seller & Admin

---
+++ src/Admin.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Admin extends User{
+    private ArrayList<Seller> sellers;
+    private ArrayList<User> users;
+
+    public Admin(String username, String name, String surname, String role) {
+        super(username, name, surname, role);
+        this.sellers = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    public ArrayList<Seller> getSellers() {
+        return sellers;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    // Μέθοδος για τη δημιουργία νέου πωλητή
+    public void createSeller(String username, String name, String surname, String company) {
+        Seller newSeller = new Seller(username, name, surname, "seller", company);
+        sellers.add(newSeller);
+        users.add(newSeller);
+        System.out.println("Δημιουργήθηκε νέος πωλητής: " + username);
+    }
+
+    // Μέθοδος για τη διαγραφή πωλητή
+    public void deleteSeller(Seller seller) {
+        sellers.remove(seller);
+        users.remove(seller);
+        System.out.println("Ο πωλητής " + seller.getUsername() + " διαγράφηκε.");
+    }
+
+    // Μέθοδος για τη δημιουργία νέου χρήστη
+    public void createUser(String username, String name, String surname, String role) {
+        User newUser = new User(username, name, surname, role);
+        users.add(newUser);
+        System.out.println("Δημιουργήθηκε νέος χρήστης: " + username);
+    }
+
+    // Μέθοδος για τη διαγραφή χρήστη
+    public void deleteUser(User userThis) {
+        users.remove(userThis);
+        System.out.println("Ο χρήστης " + userThis.getUsername() + " διαγράφηκε.");
+    }
+
+    // Μέθοδος για τη δημιουργία νέου προγράμματος
+    public void createProgram(String programName) {
+        System.out.println("Δημιουργήθηκε νέο πρόγραμμα: " + programName);
+    }
+}
--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
+
 public class Client extends User {
     private final String AFM;
     private PhoneNumber phoneNumber;
-
+    private double balance;
+    private ArrayList<String> callHistory;
     public Client(String username, String name, String surname, String role, String AFM, PhoneNumber phoneNumber) {
         super(username, name, surname, role);
         this.AFM = AFM;
         this.phoneNumber = phoneNumber;
-    }
+        this.balance = 0.0; // Αρχικό υπόλοιπο λογαριασμού
+        this.callHistory = new ArrayList<>();
 
+    }
     public String getAFM() {
         return AFM;
     }
@@ -20,6 +25,20 @@ public class Client extends User {
         this.phoneNumber = phoneNumber;
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public ArrayList<String> getCallHistory() {
+        return callHistory;
+    }
+    // Προσθήκη ενός τηλεφωνικού κλήση στο ιστορικό
+    public void addCall(String callDetails) {
+        callHistory.add(callDetails);
+    }
     @Override
     public void register() {
         super.register();
@@ -39,4 +58,30 @@ public class Client extends User {
              return number;
          }
      }
+
+    // Εμφάνιση λογαριασμού
+    public void displayAccount() {
+        System.out.println("Όνομα: " + getName() + " " + getSurname());
+        System.out.println("Τηλέφωνο: " + phoneNumber.getNumber());
+        System.out.println("ΑΦΜ: " + AFM);
+        System.out.println("Υπόλοιπο λογαριασμού: " + balance);
+    }
+
+    // Εκτύπωση του ιστορικού των κλήσεων
+    public void displayCallHistory() {
+        System.out.println("Ιστορικό κλήσεων:");
+        for (String call : callHistory) {
+            System.out.println(call);
+        }
+    }
+
+    // Μέθοδος εξόφλησης λογαριασμού
+    public void payBill(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            System.out.println("Επιτυχής εξόφληση. Νέο υπόλοιπο: " + balance);
+        } else {
+            System.out.println("Η εξόφληση απέτυχε ή το ποσό είναι μη έγκυρο.");
+        }
+    }
 }
