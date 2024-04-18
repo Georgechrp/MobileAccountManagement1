47c41e7e0b0e41fc17ec44b9ad954cc749bd8dd1
ZafDimitris <167342498+ZafDimitris@users.noreply.github.com>
2024-04-18 13:36:40 +0300

    Add files via upload

---
+++ MobileAccountManagement1-master/src/mainpackage/PhoneNumber.java
@@ -0,0 +1,23 @@
+package mainpackage;
+
+public class PhoneNumber {
+    private String number; //ο μοναδικός αριθμός τηλεφώνου
+    private Program program; //το πρόγραμμα χρέωσης (πακέτο) που αντιστοιχεί στον αριθμό τηλεφώνου
+
+    public PhoneNumber(String number, Program program) {
+        this.number = number;
+        this.program = program;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Program getProgram() {
+        return program;
+    }
+
+    public void setProgram(Program program) {
+        this.program = program;
+    }
+}
