a5ba61c09faae58c5fca93ae414560dd7865f2bf
StavKolouas <120797030+StavKolouas@users.noreply.github.com>
2024-05-29 11:18:15 +0300

    Add files via upload

---
+++ UsersFile.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users
+	(username	varchar(20),
+	 password 	varchar(20),
+	 first_name	varchar(20),
+	 surname	varchar(20),
+	 role	    varchar(20),
+	 primary key (username)
+	);
+	
+CREATE TABLE sellers
+	(company varchar(20),
+	 username	varchar(20)
+	);	
+
+CREATE TABLE clients
+	(username	varchar(20),
+	 balance numeric(20,2),
+	 phone_number varchar(20),
+	 AFM varchar(11)
+	);	
+
+CREATE TABLE bills
+	(username varchar(20),
+	 number_of_calls numeric(10,0),
+	 phone_number numeric(10,0),
+	 billing_month smallint
+	);	
+
+CREATE TABLE calls
+	(startTime timestamp,
+	 endTime timestamp,
+	 caller_phone_number varchar(20),
+	 receiver_phone_number varchar(20)
+	);
+	
+CREATE TABLE programs
+	(base_charge double precision,
+	 additional_charge double precision,
+	 minutes integer,
+	 program_name varchar(20)
+	);
