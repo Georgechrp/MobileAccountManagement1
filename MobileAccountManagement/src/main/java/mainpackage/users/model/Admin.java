d2169c5ffd48f12c51415129fc41769e7bb9ce02
stelios2002 <79938349+stelios2002@users.noreply.github.com>
2024-06-13 13:51:58 +0300

    Delete MobileAccountManagement directory

--- MobileAccountManagement/src/main/java/mainpackage/users/model/Admin.java
+++
@@ -1,8 +0,0 @@
-package mainpackage.users.model;
-
-public class Admin extends User{
-
-    public Admin(String username, String name, String surname, String password, int role) {
-        super(username, name, surname, password, role);
-    }
-}
