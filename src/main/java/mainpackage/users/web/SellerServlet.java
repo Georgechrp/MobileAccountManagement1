644dddc68793672e4c66e975901144fd65b843f0
GEORGIOS KONSTANTINOS BOUTSIKOS <giorgosmpou2002@gmail.com>
2024-06-20 15:25:11 +0300

    Add files via upload

--- src/main/java/mainpackage/users/web/SellerServlet.java
+++ src/main/java/mainpackage/users/web/SellerServlet.java
@@ -2,18 +2,22 @@ package mainpackage.users.web;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import mainpackage.users.dao.ClientDao;
 import mainpackage.users.dao.SellerDao;
+import mainpackage.users.model.Client;
 import mainpackage.users.model.Seller;
 import mainpackage.utils.dao.PhoneNumberDao;
 import mainpackage.utils.dao.ProgramDao;
+import mainpackage.utils.model.PhoneNumber;
+import mainpackage.utils.model.Program;
 
 
 @WebServlet("/SellerServlet")
@@ -34,21 +38,24 @@ public class SellerServlet extends HttpServlet {
 		String action = request.getServletPath();
 		try {
 			switch (action) {
-			case "/register":
+			case "register":
 				register(request, response);
 				break;
-			case "/login":
+			case "login":
 				UserServlet userServlet = (UserServlet) getServletContext().getAttribute("userServlet");
 		        userServlet.login(request, response);
 		        break;
-			case "/logout":
+			case "logout":
 				UserServlet userServlet1 = (UserServlet) getServletContext().getAttribute("userServlet");
 		        userServlet1.logout(request, response);
 				break;		
-			case "/display_programs":
+			case "display_programs":
 				display_programs(request, response);
 				break;
-			case "/matchClient":
+			case "insertNewClient":
+				insertNewClient(request, response);
+				break;
+			case "matchClient":
 				matchClient(request, response);
 				break;	
 			}
@@ -67,20 +74,40 @@ public class SellerServlet extends HttpServlet {
 		int role = 3;
 		Seller newSeller = new Seller(username, name, surname, password, role, company);
 		sellerDao.insertSeller(newSeller);
-		response.sendRedirect("list");
+		response.sendRedirect("SellerMain.jsp");
 	}
 	
 	
 	private void display_programs(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
-		ArrayList<Program> programs = programDao.getPrograms();
+		ArrayList <Program> programs = programDao.getPrograms();
 		for(int i=0; i < programs.size(); i++) {
-			//Here will be html code
+			
 		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher("account.jsp");
 		dispatcher.forward(request, response);
 	}
 	
-	private void matchClient(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+	private void insertNewClient(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		String username = request.getParameter("username");
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		String password = request.getParameter("password");
+		String AFM = request.getParameter("AFM");
+		Double balance = Double.parseDouble(request.getParameter("balance"));
+		PhoneNumber PhoneNumber = new PhoneNumber(request.getParameter("phonenumber"), null);
+		int role = 2;
+		Client newClient = new Client(username, name, surname, password, role,  AFM, balance, PhoneNumber);
+		clientDao.insertClient(newClient);
+		response.sendRedirect("SellerMain.jsp");
+		}
+	
+	private void matchClient(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+		ArrayList <Client> clients = clientDao.getClient();
+		for(int i=0; i < clients.size(); i++) {
+			
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(".jsp");
+		dispatcher.forward(request, response);
 
 	}
 
