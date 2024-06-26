8852ef7ce1cea7f99e36c64a35b4527d97de0186
GEORGIOS KONSTANTINOS BOUTSIKOS <giorgosmpou2002@gmail.com>
2024-06-26 14:54:29 +0300

    Add files via upload

--- src/main/java/mainpackage/utils/dao/CallDao.java
+++ src/main/java/mainpackage/utils/dao/CallDao.java
@@ -3,18 +3,28 @@ package mainpackage.utils.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import mainpackage.utils.model.Call;
+import mainpackage.utils.model.Program;
+
 
 public class CallDao {
     private static final String jdbcURL = "jdbc:mysql://localhost:3306/mobilemanagementdb";
 	private String jdbcUsername = "root";
 	private String jdbcPassword = "L1ok3y20";
 
-	private static final String INSERT_CALL_SQL = "INSERT INTO calls" 
+	private static final String INSERT_CALL_SQL = "INSERT INTO call" 
 	+ "  (call_id, startTime, endTime, caller_phone_number, receiver_phone_number) VALUES (?, ?, ?, ?, ?); ";
+	
+	private static final String GET_CALLS_SQL = "SELECT * FROM call; ";
 	
 	
 
@@ -36,7 +46,7 @@ public class CallDao {
 		return connection;
 	}
 
-	public void insertUser(Call call) throws SQLException {
+	public void insertCall(Call call) throws SQLException {
 		System.out.println(INSERT_CALL_SQL);
 		// try-with-resource statement will auto close the connection.
 		try (Connection connection = getConnection();
@@ -53,5 +63,30 @@ public class CallDao {
 		} catch (SQLException e) {
 			System.out.println(e.getStackTrace());
 		}
+	}
+
+	
+	public ArrayList <Call> getCalls() throws SQLException {
+		System.out.println(GET_CALLS_SQL);
+		ArrayList<Call> calls = new ArrayList<Call>();
+		try (Connection connection = getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(GET_CALLS_SQL);
+				ResultSet resultSet = preparedStatement.executeQuery()) {
+				while(resultSet.next()) {
+					String callerPhoneNumber = resultSet.getString("callerPhoneNumber");
+					String receiverPhoneNumber = resultSet.getString("receiverPhoneNumber");
+					LocalDateTime startTime = LocalDateTime.now();
+					LocalDateTime endTime = LocalDateTime.now();
+					String callId = resultSet.getString("callId");
+					Call cl1 = new Call(String callerPhoneNumber, String receiverPhoneNumber, LocalDateTime startTime, LocalDateTime endTime, String callId);
+					calls.add(cl1);
+				}
+				System.out.println(preparedStatement);
+			} catch (SQLException e) {
+				System.out.println(e.getStackTrace());
+			}
+		
+		
+		return calls;
 	}
 }
