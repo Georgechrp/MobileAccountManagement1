c3144263468fddb3bb10ca451e3de5fda2218ecc
StavKolouas <120797030+StavKolouas@users.noreply.github.com>
2024-07-16 13:12:37 +0300

    Update ClientServlet.java

--- src/main/java/mainpackage/users/web/ClientServlet.java
+++ src/main/java/mainpackage/users/web/ClientServlet.java
@@ -2,7 +2,6 @@ package mainpackage.users.web;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -10,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import mainpackage.users.dao.ClientDao;
 import mainpackage.users.model.Client;
@@ -19,8 +19,6 @@ import mainpackage.utils.dao.PhoneNumberDao;
 import mainpackage.utils.dao.ProgramDao;
 import mainpackage.utils.model.Call;
 import mainpackage.utils.model.PhoneNumber;
-import mainpackage.utils.model.Program;
-
 
 public class ClientServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -48,19 +46,12 @@ public class ClientServlet extends HttpServlet {
 		            case "register":
 		                System.out.println("Register action called");
 		                registerClient(request, response);
-		                break;	
-		            case "Show Account":
-		            	System.out.println("Show Account action called");
-		                display_account(request, response);
 		                break;
-		            case "Show Call History":    
-		            	System.out.println("Show Call History action called");
-		                display_call_history(request, response);
+		            case "Call history":
+		                System.out.println("Call history action called");
+		                displayCallHistory(request, response);
 		                break;
-		            case "Pay Bill":    
-		            	System.out.println("Pay Bill action called");
-		                pay_bill(request, response);
-		                break;
+		            // other cases...
 		            default:
 		                response.getWriter().println("Unknown action: " + action);
 		                break;
@@ -79,6 +70,7 @@ public class ClientServlet extends HttpServlet {
 	                case "listUser":
 	                    listUser(request, response);
 	                    break;
+	                // Add more cases as needed
 	                default:
 	                    response.sendRedirect("index.jsp");
 	                    break;
@@ -87,17 +79,15 @@ public class ClientServlet extends HttpServlet {
 	            throw new ServletException(e);
 	        }
 	    }
-	 
-	 
 	
 	private void registerClient(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
 		String username = request.getParameter("username");
-		String name = request.getParameter("first_name"); 
+		String name = request.getParameter("first_name"); // Correct parameter name
 		String surname = request.getParameter("surname");
 		String password = request.getParameter("password");
 		String AFM = request.getParameter("afm");
-		Double balance = 0.0;
-		PhoneNumber phoneNumber = new PhoneNumber(request.getParameter("phone_number"), null); 
+		Double balance = 0.0; // Initialize balance to 0.0 as it's not provided in the form
+		PhoneNumber phoneNumber = new PhoneNumber(request.getParameter("phone_number"), null); // Correct parameter name
 		int role = 2;
 		Client newClient = new Client(username, name, surname, password, role, AFM, balance, phoneNumber);
 		clientDao.insertClient(newClient);
@@ -105,28 +95,61 @@ public class ClientServlet extends HttpServlet {
 	    dispatcher.forward(request, response);
 	}
 	
-
+	
+	private void display_programs(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("programs.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	private void change_program(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("programs.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	private void display_balance(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("balance.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	private void set_balance(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("balance.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	private void displayCallHistory(HttpServletRequest request, HttpServletResponse response)
+	        throws SQLException, IOException, ServletException {
+		 HttpSession session = request.getSession(false);
+		    if (session == null || session.getAttribute("username") == null) {
+		        response.sendRedirect("login.jsp");
+		        return;
+		    }
+	    String phoneNumber = request.getParameter("phone_number");
+	    List<Call> callHistory = callDao.getCallHistory(phoneNumber);
+	    request.setAttribute("callHistory", callHistory); // Ensure this line is correct
+	    RequestDispatcher dispatcher = request.getRequestDispatcher("CallHistory.jsp");
+	    dispatcher.forward(request, response);
+	}
+	
+	
 	private void display_account(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("account.jsp");
 		dispatcher.forward(request, response);
 	}
 	
 	
-	private void display_call_history(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException{
-		ArrayList <Call> calls = callDao.getCalls();
-		request.setAttribute("calls", calls);
-		request.getRequestDispatcher("ShowCalls.jsp").forward(request, response);		
-	}
-	
-	
-	
-	
-    private void pay_bill(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException,  ServletException {
-    	RequestDispatcher dispatcher = request.getRequestDispatcher("pay_bill.jsp");
-		dispatcher.forward(request, response);
+    private void pay_bill(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		// TODO Auto-generated method stub
+		
 	}
     
-    
+    public ClientServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
     
     private void listUser(HttpServletRequest request, HttpServletResponse response)
 	        throws SQLException, IOException, ServletException {
@@ -134,10 +157,5 @@ public class ClientServlet extends HttpServlet {
 	    RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
 	    dispatcher.forward(request, response);
 	}
-    
-    
-    public ClientServlet() {
-        super();
-      
-    }
+
 }
