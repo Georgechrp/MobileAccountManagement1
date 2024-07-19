9aa4f31db6b22ffb8c301dde4a3d481606f8809f
stelios2002 <79938349+stelios2002@users.noreply.github.com>
2024-07-19 15:20:56 +0300

    Update mobilemanagementdb.sql

--- src/mobilemanagementdb.sql
+++ src/mobilemanagementdb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE `mobilemanagementdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-CREATE TABLE `user` (
+CREATE TABLE `mobilemanagementdb`.`user` (
   `username` varchar(45) NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `surname` varchar(45) NOT NULL,
@@ -9,14 +9,14 @@ CREATE TABLE `user` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `seller` (
+CREATE TABLE `mobilemanagementdb`.`seller` (
   `username` varchar(45) NOT NULL,
   `company` varchar(45) NOT NULL,
   PRIMARY KEY (`username`),
   CONSTRAINT `seller_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `program` (
+CREATE TABLE `mobilemanagementdb`.`program` (
   `id` int NOT NULL,
   `name` varchar(45) NOT NULL,
   `minutes` int NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE `program` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `phone_number` (
+CREATE TABLE `mobilemanagementdb`.`phone_number` (
   `programid` int NOT NULL,
   `number` varchar(15) NOT NULL,
   PRIMARY KEY (`number`),
@@ -33,7 +33,7 @@ CREATE TABLE `phone_number` (
   CONSTRAINT `programid` FOREIGN KEY (`programid`) REFERENCES `program` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `client` (
+CREATE TABLE `mobilemanagementdb`.`client` (
   `username` varchar(45) NOT NULL,
   `afm` varchar(12) NOT NULL,
   `balance` double NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE `client` (
   CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `call` (
+CREATE TABLE `mobilemanagementdb`.`call` (
   `caller` varchar(15) NOT NULL,
   `reciever` varchar(15) NOT NULL,
   `start_time` time NOT NULL,
@@ -55,3 +55,18 @@ CREATE TABLE `call` (
   CONSTRAINT `caller` FOREIGN KEY (`caller`) REFERENCES `client` (`phone_number`),
   CONSTRAINT `reciever` FOREIGN KEY (`reciever`) REFERENCES `client` (`phone_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `mobilemanagementdb`.`bill` (
+  `bill_id` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `billing_month` int NOT NULL,
+  `number_of_calls` int NOT NULL,
+  `paid` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`bill_id`),
+  KEY `fk_user` (`username`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `mobilemanagementdb`.`program` (`id`, `name`, `minutes`, `basecharge`, `additionalcharge`)
+VALUES (0, 'null', 0, 0, 0);
+
